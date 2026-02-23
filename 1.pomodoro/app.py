571b8ac2cd8b3e8b86c96374cafe458bb36ebc9c
DevTakas <t_o_19880522@outlook.com>
2026-02-23 07:41:58 +0000

    ポモドーロタイマー機能を追加

--- 1.pomodoro/app.py
+++ 1.pomodoro/app.py
@@ -1,1 +1,16 @@
-# Pomodoro Timer App
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
